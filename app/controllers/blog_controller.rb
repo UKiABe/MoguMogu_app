d0575ae367879766b5299ced26a7ae484ccd5f59
UkiAbe <unlimited.blade.works22@gmail.com>
2018-11-24 11:29:05 +0900

    ブログ投稿の一覧表示でHTMLのエスケープ処理を無効にした

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,15 +1,15 @@
 class BlogController < ApplicationController
   def index
-    @blogs = Blog.all.order(created_at: :asc)
+    @blogs = Blog.all.order(created_at: :desc)
   end
 
   def blog_category
-    @blogs = Blog.where(category_id: 1).order(created_at: :asc)
+    @blogs = Blog.where(category_id: 1).order(created_at: :desc)
     render 'blog/category'
   end
 
   def dish_category
-    @blogs = Blog.where(category_id: 2).order(created_at: :asc)
+    @blogs = Blog.where(category_id: 2).order(created_at: :desc)
     render 'blog/category'
   end
 
